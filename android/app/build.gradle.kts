5617b96b9289a6c1aa6ad95c5fe4f88341fbd929
Mete Baykal <74907199+Liosdali@users.noreply.github.com>
2026-07-31 19:18:07 +0300

    Day 1 path added

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,7 +20,7 @@ if (hasReleaseKeystore) {
 }
 
 android {
-    namespace = "com.exercise_app.exercise_app"
+    namespace = "com.MythosForgeLabs.AtlasWorkout"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -34,7 +34,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.exercise_app.exercise_app"
+        applicationId = "com.MythosForgeLabs.AtlasWorkout"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
